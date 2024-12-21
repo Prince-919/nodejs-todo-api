49c6361f088a7072995c8c331f263ddd7399b9e5
Prince Sharma <86801714+Prince-919@users.noreply.github.com>
2024-12-21 15:06:48 +0530

    add cors packages to allow access api

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { dbConnect } from "./config";
 import noteRouter from "./routes";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
